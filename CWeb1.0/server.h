ba53652836c3f50d00afa6d16f2b4bfa45b445d7
LauWai0032 <lau_wai@icloud.com>
2025-11-28 17:48:27 +0800

    20251128-2update

--- CWeb1.0/server.h
+++ CWeb1.0/server.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <arpa/inet.h>
 #include <sys/epoll.h>
+#include <fcntl.h>
 int initListenFD(unsigned short port);
 int epollRun(int lfd);
-int acceptConn(int epfd, int lfd);
-
+int acceptConn(int lfd, int epfd);
